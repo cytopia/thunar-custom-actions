bd5296cab26df7294cc8694259e6f695e26fc270
Matias Wilkman <matias.wilkman@gmail.com>
2015-12-04 15:34:29 +0200

    updated to pass automated tests
    Made at meatspace coordinates
    Latitude:  61.44584529999999
    Longitude:  23.8461139

--- thunar-upload-to-imgur.sh
+++ thunar-upload-to-imgur.sh
@@ -106,7 +106,7 @@ if ! command -v curl >/dev/null 2>&1 ; then
 	exit 1
 fi
 
-TITLE='Uploading to Imgur...'`basename "${f}"` 
+TITLE='Uploading to Imgur...'$(basename "${f}")
 
 IMGUR_KEY="4907fcd89e761c6b07eeb8292d5a9b2a"
 TMPFILE=$(mktemp)
@@ -115,20 +115,20 @@ TMPFILE=$(mktemp)
 [ ! -z "${h##*[!0-9]*}" ]	&& HEIGHT=$h	|| HEIGHT=140
 [ -n "${t}" ]				&& TITLE="${t}"		|| TITLE="Uploading to imgur: $(basename "${f}")"
 
-curl -# -F "image"=@"$f" -o ${TMPFILE} -F "key"=${IMGUR_KEY} http://imgur.com/api/upload.xml 2>&1 | gawk -v RS='\r' '{print $2; fflush("") }' | zenity --width=${WIDTH} --height=${HEIGHT} --progress --title="${TITLE}" --text="${TITLE}" --auto-close --time-remaining 
+curl -# -F "image"=@"$f" -o "${TMPFILE}" -F "key"=${IMGUR_KEY} http://imgur.com/api/upload.xml 2>&1 | gawk -v RS='\r' '{print $2; fflush("") }' | zenity --width="${WIDTH}" --height="${HEIGHT}" --progress --title="${TITLE}" --text="${TITLE}" --auto-close --time-remaining 
 #curl -# -F "image"=@"$f" -F "key"="4907fcd89e761c6b07eeb8292d5a9b2a" http://imgur.com/api/upload.xml | grep -Eo "[0-9]{1,3}" | zenity --width=${WIDTH} --height=${HEIGHT} --progress --title="${TITLE}" --text="${TITLE}" --auto-close --time-remaining
 #stdbuf -oL tr $'\r' $'\n' | stdbuf -oL grep --line-buffered -Eo '([0-9]+)\.[0-9]%$' | zenity --width=${WIDTH} --height=${HEIGHT} --progress --title="${TITLE}" --text="${TITLE}" --auto-close --time-remaining 
 
 ########################## gui output ###############################
-[ -n "${t}" ]				&& TITLE=$t		|| TITLE="Uploaded to imgur: `basename "${f}"`"
+[ -n "${t}" ]				&& TITLE=$t		|| TITLE="Uploaded to imgur: $(basename "${f}")"
 
 #TEXT=$(curl -F "image"=@"$f" -F "key"="a3793a1cce95f32435bb002b92e0fa5e" http://imgur.com/api/upload.xml | sed -e "s/.*<imgur_page>//" | sed -e "s/<.*//")
 
 #TEXT='<?xml version="1.0" encoding="utf-8"?> <rsp stat="ok"><image_hash>d5gSMGf</image_hash><delete_hash>doB1PJ99oDkMiKm</delete_hash><original_image>http://i.imgur.com/d5gSMGf.png</original_image><large_thumbnail>http://i.imgur.com/d5gSMGfl.jpg</large_thumbnail><small_thumbnail>http://i.imgur.com/d5gSMGfs.jpg</small_thumbnail><imgur_page>http://imgur.com/d5gSMGf</imgur_page><delete_page>http://imgur.com/delete/doB1PJ99oDkMiKm</delete_page></rsp>'
 
-TEXT=$(cat ${TMPFILE})
+TEXT=$"(cat ${TMPFILE})"
 #echo ${TEXT}
-rm ${TMPFILE}
+rm $"{TMPFILE}"
 
 #TEXT="$(curl -# -F "image"=@"${f}" -F "key=${IMGUR_KEY}" http://imgur.com/api/upload.xml)"
 TAG="$(echo "${TEXT}" | grep -Eo '<[a-z_]+>http' | sed -e "s/http//" | sed -e "s/<//" | sed -e "s/>//")"
