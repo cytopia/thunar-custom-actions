d8b7a438d6e7c976b11e9e846a728bebe24e3608
cytopia <cytopia@everythingcli.org>
2017-09-29 20:07:20 +0200

    TCA-015 Fixing #19 Algorithm field grayed out

--- thunar-openssl-encrypt-decrypt.sh
+++ thunar-openssl-encrypt-decrypt.sh
@@ -78,8 +78,7 @@ fi
 
 
 chooseOptions () {
-	algorithms=$(openssl enc --help 2>&1 \
-		| grep -A 200 'Cipher Types' \
+	algorithms=$(openssl enc -ciphers 2>&1 \
 		| grep '^-' \
 		| xargs \
 		| sed -e 's/^-//' -e 's/ -/|/g')
