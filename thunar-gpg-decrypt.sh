7a797a63402b375a84662995d3bf45a10e1b2a37
user414 <lotusom-dev@outlook.com>
2023-12-28 09:05:20 -0400

    use common functions script and add support for asc extensions which is now the default for gpg encrypt

--- thunar-gpg-decrypt.sh
+++ thunar-gpg-decrypt.sh
@@ -35,45 +35,17 @@
 #  https://github.com/cytopia/thunar-custom-actions
 #
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
 
-usage() {
-	echo "$0 -f <filename>"
-	echo
-	echo " required:"
-	echo "   -f    input filename"
-	echo
-}
+source $DIR/thunar-gpg-functions.sh
 
-
-while getopts ":f:" i; do
-	case "${i}" in
-		f)
-			f=${OPTARG}
-			;;
-		*)
-			echo "Error - unrecognized option $1" 1>&2;
-			usage
-			;;
-	esac
-done
-shift $((OPTIND-1))
-
-# Check if file is specified
-if [ -z "${f}" ]; then
-	echo "Error - no file specified" 1>&2;
-	usage
-	exit 1
-fi
-
-# Check if gpg exists
-if ! command -v gpg >/dev/null 2>&1 ; then
-	echo "Error - 'gpg' not found." 1>&2
-	exit 1
-fi
+checkCommandLineArg
+checkFileArg
+checkBinaryReq
 
 
 # remove ".gpg" file extension if it exists
-output="$( echo "${f}" | sed 's/\.gpg$//g' )"
+output="$( echo "${f}" | sed 's/\(\.gpg\|\.asc\)$//g' )"
 
 $(which gpg) -o "${output}" -d "${f}"
 exit $?
